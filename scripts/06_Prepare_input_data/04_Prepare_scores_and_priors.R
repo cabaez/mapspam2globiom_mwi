93c54f8e4c3f41016528909ad29d9204a245acc0
michielvandijk <michiel.vandijk@wur.nl>
2020-05-08 10:34:33 +0200

    Change order of scripts

--- scripts/06_Prepare_input_data/04_Prepare_scores_and_priors.R
+++ scripts/06_Prepare_input_data/04_Prepare_scores_and_priors.R
@@ -9,10 +9,6 @@
 source(here::here("scripts/01_model_setup/01_model_setup.r"))
 
 
-############### HARMONIZE INPUT DATA ###############
-harmonize_inputs(param)
-
-
 ############### PREPARE BIOPHYSICAL SUITABILITY AND POTENTIAL YIELD ###############
 prepare_bs_yg("biophysical_suitability", param)
 prepare_bs_yg("potential_yield", param)
@@ -21,7 +17,4 @@ prepare_bs_yg("potential_yield", param)
 ############### PREPARE SCORE ###############
 prepare_score(param)
 
-
-############### COMBINE MODEL INPUTS ###############
-combine_inputs(param)
 
